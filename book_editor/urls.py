30b0bcd44aa3b5a51107519defcc3782c6194643
zahur <zahurmeerun@hotmail.com>
2022-09-03 16:03:35 +0400

    setup: complete for base settings with home page

--- book_editor/urls.py
+++ book_editor/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# for media files setting
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", include("home.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
