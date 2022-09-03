b4f415abff6331ae09778465bc8418c27f6d8203
zahur <zahurmeerun@hotmail.com>
2022-09-03 15:55:14 +0400

    add: base template and static setup

--- book_editor/settings.py
+++ book_editor/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = "book_editor.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+
+# For main static file not tied up to app and in base_dir/Required for base.css so django will look here too
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+# This will add the file to the media folder and not to another url
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
